9e8db17829628da528b4823e8124e87322ee3056
saif88888 <saif_saeed_cs@yahoo.com>
2020-06-24 22:40:40 +0100

    Updates

---
+++ precache-manifest.ee3c00f208fbabcd32c046adf51d93e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990abaaef8c973bfcb7eea58b002246f",
+    "url": "/react-deplo/index.html"
+  },
+  {
+    "revision": "a2babb43ecf5c55c96f5",
+    "url": "/react-deplo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c899c34279c44a9fd05f",
+    "url": "/react-deplo/static/js/2.25c6addb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deplo/static/js/2.25c6addb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2babb43ecf5c55c96f5",
+    "url": "/react-deplo/static/js/main.545b159a.chunk.js"
+  },
+  {
+    "revision": "156a2794b51e212a36ff",
+    "url": "/react-deplo/static/js/runtime-main.9b8a73cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deplo/static/media/logo.5d5d9eef.svg"
+  }
+]);
